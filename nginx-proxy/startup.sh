4e9b0521ba681b4114dd046e4380a6ea1dc2e822
Eckart Burgwedel <eckart@uberchord.com>
2018-06-16 01:20:35 +0200

    Rebuild entire architecture to Alpine, nginx, PHP, wordpress, nginx proxy

---
+++ nginx-proxy/startup.sh
@@ -0,0 +1,23 @@
+#
+# startup.sh
+#
+
+# Startup the nginx server. The server has to be active for the Let's Encrypt Certbot to
+# register and install the certificates.
+nginx -g "daemon on;"
+
+# Checks that the SSL certificates are installed. If they are, renews any that are old, and
+# installs them if not.
+if [[ -d "/etc/letsencrypt/live/skillstack.io" ]]; then
+        certbot renew --quiet
+else
+        if ! [[ -d "/etc/letsencrypt/live/skillstack.io" ]]; then
+                certbot --nginx -m eckart@skillstack.io --agree-tos --no-eff-email --redirect --expand -d example.com,www.example.com
+        fi
+        if ! [[ -f "/etc/ssl/certs/dhparam.pem" ]]; then
+                openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+        fi
+fi
+
+# Shuts down the daemonized nginx server and fires up one in the foreground.
+nginx -s stop && nginx -g 'daemon off;'
